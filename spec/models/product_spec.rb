e3e5aa2a531f3c86eb837d8d0ae2d94bede54526
BlessedPages <jacques.leroux3@gmail.com>
2017-08-03 16:34:31 +0200

    6.2 rspec validation and average rating test

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe Product do
+  
+  context "when the product has comments" do
+    before do
+      product.comments.create!(rating: 1, user: user, body: "horrible hoodie")
+      product.comments.create!(rating: 3, user: user, body: "stylish but cold")
+      product.comments.create!(rating: 5, user: user, body: "my favourite hoodie")
+    end
+
+    let(:product) { Product.create!(name: "Hoodie") }
+    let(:user) { User.create!(email: "testuser@rspec.com", password: "R$p3c!") }
+  
+
+    it "returns the average rating of all comments" do
+      expect(product.average_rating).to eq(3)
+    end
+  end
+
+  context "has no name assigned to product" do
+    before do
+      Product.new(description: "A very nice cow hat")
+    end
+
+    it "is not a valid product" do
+      expect(Product.new(description: "A very nice cow hat")).not_to be_valid
+    end
+  end
+end
