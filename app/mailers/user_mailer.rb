f29ab68e24b302fe37c195bc17730a6482b03b51
BlessedPages <jacques.leroux3@gmail.com>
2017-08-09 14:13:26 +0200

    6.6 create payments controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,16 @@
 class UserMailer < ApplicationMailer
-	default from: "jacques.leroux3@gmail.com"
+  default from: "jacques.leroux3@gmail.com"
 
-	def contact_form(email, name, message)
-		@message = message
-			mail(:from => email,
-						:to => "jacques.leroux3@gmail.com",
-						:subject => "A message from #{name}")
-	end
+  def contact_form(email, name, message)
+    @message = message
+      mail(:from => email,
+           :to => "jacques.leroux3@gmail.com",
+           :subject => "A message from #{name}")
+  end
 
-	def welcome(user)
-		@appname = "The Having Cows Parade"
-		mail(to: user.email,
-			subject: "Welcome to #{@appname}!")
-	end
+  def welcome(user)
+    @appname = "The Having Cows Parade"
+    mail(to: user.email,
+         subject: "Welcome to #{@appname}!")
+  end
 end
