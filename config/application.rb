a56de070840be8b7db0a5266bd2b6be71fb49e25
BlessedPages <jacques.leroux3@gmail.com>
2017-08-15 11:39:08 +0200

    6.12.2 redis corrected

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.cache_store = :redis_store, 'redis:http://localhost:6379/0/cache'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +10,8 @@ module HavingCows
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
